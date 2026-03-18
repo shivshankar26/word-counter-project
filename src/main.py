cc29d97e520b855a322be0e50ca48993ad89a316
NitishMamadgi <mamadginitish@gmail.com>
2026-03-18 16:44:46 +0530

    added screenshots and updated main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
+from collections import Counter
+
 with open("data/input.txt", "r") as f:
     text = f.read()
 
 words = text.split()
 
 print("Word count:", len(words))
+
+freq = Counter(words)
+print(freq)
